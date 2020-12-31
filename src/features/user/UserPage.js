6de09a03602d17fe6ad22ed98974f2af197d1899
viethai-git <75465111+viethai-git@users.noreply.github.com>
2020-12-31 14:41:07 +0700

    update paging

--- src/features/user/UserPage.js
+++ src/features/user/UserPage.js
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
 import { deleteUser } from './UserSlice.js';
+import { Button, Col, Label, Row, Table } from 'reactstrap';
 import PropTypes from 'prop-types';
+import Pagination from "react-js-pagination";
+import {useState} from 'react';
 
 UserPage.propTypes = {
     userList: PropTypes.array,
@@ -14,6 +17,19 @@ UserPage.defaultPros = {
 
 export default function UserPage(props) {
 	const { userList, editUserClick } = props;
+
+	const [activePage, setActivePage] = useState(1);
+	const total = userList.length;
+	const indexOfLast = activePage * 3;
+	const indexOfFirst = indexOfLast -3;
+	const currentList = userList.slice(indexOfFirst, indexOfLast); 
+
+	const handlePageChange = (pageNumber) => {
+		console.log(userList);
+		console.log(pageNumber);
+		setActivePage(pageNumber);
+	}
+
     const dispatch = useDispatch();
 
 	const handleEdit = (a) => {
@@ -25,31 +41,48 @@ export default function UserPage(props) {
         console.log('delete user')
         const action = deleteUser(values);
         dispatch(action);
-}
+	}
+
+	const colorText = {
+		color: "white"
+	}
 	return (
 		<div>
-        <Link to="/listusers/add">Add new user</Link>
-		<table className='table table-striped' aria-labelledby="tabelLabel" border="0">
-			<thead>
-				<tr>
-					<th>Id</th>
-					<th>Name</th>
-					<th>Age</th>
-                    <th></th>
-				</tr>
-			</thead>
-			<tbody>
-				{userList.map(user =>
-					<tr key={user.id}>
-						<td>{user.id}</td>
-						<td>{user.name}</td>
-						<td>{user.age}</td>
-                        <td><button onClick={() => handleEdit(user)}>Edit</button>&nbsp;&nbsp;
-                        <button onClick={() => handleDelete(user)}>Delete</button></td>
+        <Button color="primary"><Link style={colorText} to="/listusers/add">Add new user</Link></Button>
+		<Row>
+			<table className='table table-striped' aria-labelledby="tabelLabel" border="0">
+		
+				<thead>
+					<tr>
+						<th>Id</th>
+						<th>Name</th>
+						<th>Age</th>
+						<th></th>
 					</tr>
-				)}
-			</tbody>
+				</thead>
+				<tbody>
+					{currentList.map(user =>
+						<tr key={user.id}>
+							<td>{user.id}</td>
+							<td>{user.name}</td>
+							<td>{user.age}</td>
+							<td><Button color="success" onClick={() => handleEdit(user)}>Edit</Button>&nbsp;&nbsp;
+							<Button color="danger" onClick={() => handleDelete(user)}>Delete</Button></td>
+						</tr>
+					)}
+				</tbody>
 			</table>
+		
+			<Pagination
+				itemClass="page-item"
+				linkClass="page-link" 
+				activePage={activePage}
+				itemsCountPerPage={3}
+				totalItemsCount={total}
+				pageRangeDisplayed={3}
+				onChange={handlePageChange}
+			/>
+		</Row>
 		</div>
 	);
 }
