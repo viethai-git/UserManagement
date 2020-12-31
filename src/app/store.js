8bc24312f40c98bb01d3bb4e4d79566729edda28
Vũ Kỳ <vukynpro@gmail.com>
2020-12-31 11:27:11 +0700

    add redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import {combineReducers} from "redux"; 
+import storage from 'redux-persist/lib/storage';
 
 export default configureStore({
   reducer: {
