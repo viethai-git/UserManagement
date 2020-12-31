c2a73e8052a9b90922b8c954f96075e9a49f4768
Vũ Kỳ <vukynpro@gmail.com>
2020-12-31 15:32:52 +0700

    add popup delete

--- src/features/user/UserPage.js
+++ src/features/user/UserPage.js
@@ -5,24 +5,26 @@ import { deleteUser } from './UserSlice.js';
 import { Button, Col, Label, Row, Table } from 'reactstrap';
 import PropTypes from 'prop-types';
 import Pagination from "react-js-pagination";
-import {useState} from 'react';
+import { useState } from 'react';
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 UserPage.propTypes = {
-    userList: PropTypes.array,
+	userList: PropTypes.array,
 };
 
 UserPage.defaultPros = {
-    userList: []
+	userList: []
 }
 
 export default function UserPage(props) {
 	const { userList, editUserClick } = props;
-
+	const dispatch = useDispatch();
 	const [activePage, setActivePage] = useState(1);
 	const total = userList.length;
 	const indexOfLast = activePage * 3;
-	const indexOfFirst = indexOfLast -3;
-	const currentList = userList.slice(indexOfFirst, indexOfLast); 
+	const indexOfFirst = indexOfLast - 3;
+	const currentList = userList.slice(indexOfFirst, indexOfLast);
 
 	const handlePageChange = (pageNumber) => {
 		console.log(userList);
@@ -30,17 +32,15 @@ export default function UserPage(props) {
 		setActivePage(pageNumber);
 	}
 
-    const dispatch = useDispatch();
-
 	const handleEdit = (a) => {
-        console.log('a:', a);
-        if (editUserClick) editUserClick(a);
-    }
+		console.log('a:', a);
+		if (editUserClick) editUserClick(a);
+	}
 
-    const handleDelete = (values) => {
-        console.log('delete user')
-        const action = deleteUser(values);
-        dispatch(action);
+	const handleDelete = (values) => {
+		console.log('delete user')
+		const action = deleteUser(values);
+		dispatch(action);
 	}
 	const clear = () => {
 		window.localStorage.clear()
@@ -49,46 +49,66 @@ export default function UserPage(props) {
 	const colorText = {
 		color: "white"
 	}
+
+	const confirmDelete = (values) => {
+		confirmAlert({
+			//title: 'Delete id: ' + values.id,
+			message: `Are you sure to delete "${values.name}"`,
+			buttons: [
+				{
+					label: 'Yes',
+					onClick: () => handleDelete(values)
+				},
+				{
+					label: 'No'
+					// onClick: () => alert('Click No')
+				}
+			]
+		});
+	}
+
 	return (
 		<div>
-        <Button color="primary"><Link style={colorText} to="/listusers/add">Add new user</Link></Button>
+			<Button color="primary"><Link style={colorText} to="/listusers/add">Add new user</Link></Button>
 		&nbsp;
-		<Button color="danger" onClick={clear}>Clear</Button>
-		<br/><br/>
-		<Row>
-			<table className='table table-striped' aria-labelledby="tabelLabel" border="0">
-		
-				<thead>
-					<tr>
-						<th>Id</th>
-						<th>Name</th>
-						<th>Age</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					{currentList.map(user =>
-						<tr key={user.id}>
-							<td>{user.id}</td>
-							<td>{user.name}</td>
-							<td>{user.age}</td>
-							<td><Button color="success" onClick={() => handleEdit(user)}>Edit</Button>&nbsp;&nbsp;
-							<Button color="danger" onClick={() => handleDelete(user)}>Delete</Button></td>
+			<Button color="danger" onClick={clear}>Clear</Button>
+			<br /><br />
+			<Row>
+				<table className='table table-striped' aria-labelledby="tabelLabel" border="0">
+
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Name</th>
+							<th>Age</th>
+							<th></th>
 						</tr>
-					)}
-				</tbody>
-			</table>
-		
-			<Pagination
-				itemClass="page-item"
-				linkClass="page-link" 
-				activePage={activePage}
-				itemsCountPerPage={3}
-				totalItemsCount={total}
-				pageRangeDisplayed={3}
-				onChange={handlePageChange}
-			/>
-		</Row>
+					</thead>
+					<tbody>
+						{currentList.map(user =>
+							<tr key={user.id}>
+								<td>{user.id}</td>
+								<td>{user.name}</td>
+								<td>{user.age}</td>
+								<td>
+									<Button color="success" onClick={() => handleEdit(user)}>Edit</Button>&nbsp;&nbsp;
+									<Button color="danger" onClick={() => confirmDelete(user)}>Delete</Button>
+								</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+
+				<Pagination
+					itemClass="page-item"
+					linkClass="page-link"
+					activePage={activePage}
+					itemsCountPerPage={3}
+					totalItemsCount={total}
+					pageRangeDisplayed={3}
+					onChange={handlePageChange}
+				/>
+			</Row>
 		</div>
 	);
 }
